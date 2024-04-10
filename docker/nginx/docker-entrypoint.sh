846fffe35f03c6a2d2b129c6ffa855be47abe52d
jymarkb <jymark.borja@gmail.com>
2024-04-10 12:21:13 +0800

    update: docker nginx build

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -6,13 +6,19 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-    composer install --prefer-dist --no-progress --no-suggest --no-interaction
-    bin/console assets:install --no-interaction
+    # composer install --prefer-dist --no-progress --no-interaction
+    # bin/console assets:install --no-interaction
 
 	until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
 	    (>&2 echo "Waiting for db to be ready...")
-		sleep 1
+		sleep 5
 	done
-fi
 
+	if [ -f "./migrations/*.php" ]; then
+        (>&2 echo "Connection is Ready, Running Migration")
+        bin/console doctrine:migrations:migrate
+    else
+        (>&2 echo "No migrations found, skipping migration.")
+    fi
+fi
 exec "$@"
